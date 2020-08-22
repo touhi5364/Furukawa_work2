c995f252879e9c4571d832a8195ecef6e062063a
touhi5364 <39119674+touhi5364@users.noreply.github.com>
2020-08-22 16:27:21 +0900

    work2

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$('.slider').slick({
+    // autoplay:true,
+    autoplaySpeed:5000,
+    dots:true,
+    arrows:true,
+    infinite: true,
+    slidesToShow:3,
+   
+});
---
+++ js/wave.js
@@ -0,0 +1,90 @@
+﻿var myWave = $('#wave').wavify({
+    height: 40,
+    bones: 6,
+    amplitude: 12,
+    color: 'rgba(132,132,132,.6)',
+    speed: .18
+});
+var myWave = $('#wave2').wavify({
+    height: 40,
+    bones: 5,
+    amplitude: 10,
+    color: 'rgba(132,132,132,.6)',
+    speed: .15
+});
+var myWave = $('#wave3').wavify({
+    height: 40,
+    bones: 5,
+    amplitude: 20,
+    color: '#222222',
+    speed: .15
+});
+var myWave = $('#wave4').wavify({
+    height: 40,
+    bones: 5,
+    amplitude: 20,
+    color: '#222222',
+    speed: .17
+});
+var myWave = $('#wave5').wavify({
+    height: 40,
+    bones: 6,
+    amplitude: 22,
+    color: 'rgba(132,132,132,.6)',
+    speed: .30
+});
+var myWave = $('#wave6').wavify({
+    height: 40,
+    bones: 5,
+    amplitude: 20,
+    color: 'rgba(132,132,132,.6)',
+    speed: .25
+});
+var myWave = $('#wave7').wavify({
+    height: 40,
+    bones: 5,
+    amplitude: 20,
+    color: '#222222',
+    speed: .20
+});
+var myWave = $('#wave8').wavify({
+    height: 40,
+    bones: 5,
+    amplitude: 20,
+    color: '#222222',
+    speed: .20
+});
+var myWave = $('#wave9').wavify({
+    height: 40,
+    bones: 5,
+    amplitude: 22,
+    color: 'rgba(132,132,132,.6)',
+    speed: .18
+});
+var myWave = $('#wave10').wavify({
+    height: 40,
+    bones: 4,
+    amplitude: 20,
+    color: 'rgba(132,132,132,.6)',
+    speed: .20
+});
+var myWave = $('#wave11').wavify({
+    height: 40,
+    bones: 5,
+    amplitude: 20,
+    color: '#222222',
+    speed: .20
+});
+var myWave = $('#wave12').wavify({
+    height: 40,
+    bones: 4,
+    amplitude: 20,
+    color: '#222222',
+    speed: .15
+});
+// $ ('.slider').slick({
+//     // autoplay:true,
+//     // autoplaySpeed:5000,
+//     dots:true,
+//     // fade:true,
+// });
